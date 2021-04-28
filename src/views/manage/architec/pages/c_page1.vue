bfa162717dd07f11494753673542ff04f48475be
zhangyang <274684940@qq.com>
2021-04-28 22:21:00 +0800

    栏目管理bug修改

--- src/views/manage/architec/pages/c_page1.vue
+++ src/views/manage/architec/pages/c_page1.vue
@@ -1,5 +1,6 @@
 <template>
-    <div class="childpage1">
+    <div class="childpage1" >
+        <!-- 审核人弹窗 -->
         <el-dialog
             :close-on-click-modal="false"
             append-to-body
@@ -24,10 +25,10 @@
             </template>
 
             <span slot="footer" class="dialog-footer">
-                <!-- <el-button @click="dialogVisible = false">取 消</el-button> -->
                 <el-button type="primary" @click="suretransfer">确 定</el-button>
             </span>
         </el-dialog>
+
         <el-form-item label-width="150px" label="多级审核:" prop="extra.defaultform">
             <el-button @click="edituser" type="primary" size="mini">编辑审核人</el-button>
 
@@ -37,13 +38,14 @@
 
         </el-form-item>
 
+        <!-- 显示更多 -->
         <el-form-item label-width="150px" label="显示更多:" prop="extra.display_more">
             <el-radio-group v-model="c_form.extra.display_more">
                 <el-radio :label="2">显示</el-radio>
                 <el-radio :label="0">不显示</el-radio>
             </el-radio-group>
         </el-form-item>
-        
+        <!-- 显示标题 -->
         <el-form-item label-width="150px" label="显示标题:" prop="extra.display_title">
             <el-radio-group v-model="c_form.extra.display_title">
                 <el-radio :label="2">显示</el-radio>
@@ -70,17 +72,21 @@ export default({
     data () {
         return {
             dialogVisible: false,
-            // userdata:[],
             listnum:1,
-            listshowarr:[]
+            listshowarr:[],
+            p_flag:false
         }
     },
     created () {
-        // this.getuserfn();
+        // this.p_flag = true;
     },
+    // watch: {
+    //     c_form
+    // },
     computed:{
         c_form: {
             get:function() {
+                // this.p_flag = true;
                 return this.value; 
             },
             set:function(value) {
@@ -89,6 +95,7 @@ export default({
         }
     },
     methods: {
+        // 加一级
         addtransfer(){
             this.listnum++
             this.c_form.extra.multi_review.length =  this.listnum
@@ -99,6 +106,7 @@ export default({
             }
             console.log(this.c_form.extra.multi_review)
         },
+        // 删除一级
         deltransfer(){
             if(this.listnum>=1){
                 this.listnum--
@@ -116,12 +124,14 @@ export default({
             }
             console.log(this.c_form.extra.multi_review)
         },
+        // 编辑审核人
         edituser(){
             this.dialogVisible = true;
         },
         filterMethod(query, item) {
           return item.pinyin.indexOf(query) > -1;
         },
+        // 确定审核人
         suretransfer(){
             this.dialogVisible = false;
             this.listshowarr.length = this.c_form.extra.multi_review.length;
@@ -129,6 +139,7 @@ export default({
         },
         showtxt(){
             // console.log(this.userdata)
+            // this.listshowarr = [];
             for(var i=0;i<this.c_form.extra.multi_review.length;i++){
                 this.listshowarr[i] = [];
             }
@@ -136,14 +147,12 @@ export default({
                 for(var j=0;j<this.c_form.extra.multi_review[i].length;j++){//内数组
                     for(var k=0;k<this.userdata.length;k++){
                         if(this.c_form.extra.multi_review[i][j]==this.userdata[k].key){
-                            // console.log(1)
                             this.listshowarr[i][j] = this.userdata[k].label
                         }
                     }
                 }
             }
             this.$forceUpdate()
-            // console.log(this.c_form.extra.multi_review)
             // console.log(this.listshowarr)
         },
     }
