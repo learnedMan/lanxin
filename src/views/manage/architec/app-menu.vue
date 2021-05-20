d656de05c0ff682ceb295635bde15a5b06e9762e
libin <18657190903@163.com>
2021-05-20 17:09:56 +0800

    APP用户中心菜单

---
+++ src/views/manage/architec/app-menu.vue
@@ -0,0 +1,18 @@
+<style type="text/scss" lang="scss" scoped>
+
+</style>
+<template>
+    <div class="xl-app-menu">
+      APP用户中心菜单
+    </div>
+</template>
+
+<script>
+    export default {
+      data() {
+        return {
+
+        }
+      }
+    }
+</script>
