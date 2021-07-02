e171307e8978137b7b25362639b2680727584ee8
zhangyang <274684940@qq.com>
2021-07-02 16:50:33 +0800

    秘钥管理的appid允许输入英文

--- src/views/manage/architec/column.vue
+++ src/views/manage/architec/column.vue
@@ -286,7 +286,7 @@
             </el-form-item>
 
             <!-- 默认 -->
-            <child-page1 v-if="forceRefresh" v-show="form.type=='default'||form.type=='video_waterfall'||form.type=='video_landscape'||form.type=='subscription'" ref="c_page1" :userdata="userdata"  :form="form" v-model="form" />
+            <child-page1 v-if="forceRefresh" v-show="form.type=='default'||form.type=='video_waterfall'||form.type=='video_landscape'||form.type=='subscription'||form.type=='topic'||form.type=='matrix'" ref="c_page1" :userdata="userdata"  :form="form" v-model="form" />
             <!-- 服务 -->
             <el-form-item v-show="form.type=='service'" label-width="150px" label="服务背景图:" prop="extra.background">
               <el-upload
