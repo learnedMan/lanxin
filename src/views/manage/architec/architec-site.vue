6a46a917e9919ec995bc4788b091ffe22b7e824d
zhangyang <274684940@qq.com>
2021-07-06 15:17:17 +0800

    站点管理新增字段

--- src/views/manage/architec/architec-site.vue
+++ src/views/manage/architec/architec-site.vue
@@ -60,100 +60,108 @@
       @pagination="getList"
     />
     <!-- 新增/修改站点弹窗 -->
-    <el-dialog width="700px"
+    <el-dialog width="1400px"
     :close-on-click-modal = false
     :title="dialogTitle" :visible.sync="dialogFormVisible">
-      <el-form :model="form" :rules="rules" ref="dataForm">
-        <el-form-item  label-width="120px" label="站点名称" prop="name">
-          <el-input style="width: 300px" autocomplete="off" placeholder="请输入站点名称" v-model="form.name"></el-input>
-        </el-form-item>
-        <el-form-item  label-width="120px" label="英文标识" prop="en_name">
-          <el-input style="width: 300px" autocomplete="off" placeholder="请输入英文标识" v-model="form.en_name"></el-input>
-        </el-form-item>
-        <el-form-item  label-width="120px" label="区域选择" prop="zone_id">
-            <el-select v-model="form.zone_id" placeholder="请选择">
-              <el-option v-for="item in zoneoptions" :key="item.id" :label="item.description" :value="item.id">
+      <el-form style="display:flex;" :model="form" :rules="rules" ref="dataForm">
+
+        <div class="form-l" style="width:50%;padding-left:100px;">
+          <el-form-item  label-width="120px" label="站点名称" prop="name">
+            <el-input style="width: 300px" autocomplete="off" placeholder="请输入站点名称" v-model="form.name"></el-input>
+          </el-form-item>
+          <el-form-item  label-width="120px" label="英文标识" prop="en_name">
+            <el-input style="width: 300px" autocomplete="off" placeholder="请输入英文标识" v-model="form.en_name"></el-input>
+          </el-form-item>
+          <el-form-item  label-width="120px" label="区域选择" prop="zone_id">
+              <el-select v-model="form.zone_id" placeholder="请选择">
+                <el-option v-for="item in zoneoptions" :key="item.id" :label="item.description" :value="item.id">
+                </el-option>
+              </el-select>
+          </el-form-item>
+
+          <el-form-item  label-width="120px" label="基础点击量随机" prop="extra.random_view_range">
+            <el-input-number
+            @input="forceUpdate()"
+            :precision="0"
+            :max="form.extra.random_view_range.max || 0"
+            :controls="false"
+            v-model="form.extra.random_view_range.min"></el-input-number>
+            ~
+            <el-input-number
+            @input="forceUpdate()"
+            :precision="0"
+            :min="form.extra.random_view_range.min || 0"
+            :controls="false"
+            v-model="form.extra.random_view_range.max"></el-input-number>
+          </el-form-item>
+          <el-form-item  label-width="120px" label="点击量系数" prop="extra.multiplying_factor">
+            <el-input-number
+            @input="forceUpdate()"
+            :precision="0"
+            :controls="false"
+            v-model="form.extra.multiplying_factor"></el-input-number>
+          </el-form-item>
+          <el-form-item  label-width="120px" label="LOGO："  prop="logo">
+            <el-upload
+              class="avatar-uploader"
+              :action="VUE_APP_BASE_API+'/api/upload/image'"
+              :headers="importHeaders"
+              name="image"
+              :show-file-list="false"
+              :on-success="handleAvatarSuccess"
+              :before-upload="beforeAvatarUpload">
+              <img v-if="form.logo" :src="form.logo" class="avatar">
+              <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+            </el-upload>
+          </el-form-item>
+          <el-form-item  label-width="120px" label="水印："  prop="extra.watermark">
+            <upload-single v-model="form.extra.watermark" />
+          </el-form-item>
+        </div>
+        <div class="form-r" style="width:50%;padding-left:50px;">
+          <el-form-item  label-width="120px" label="分享页域名" prop="extra.share_page_domain">
+            <el-input style="width: 300px" autocomplete="off" placeholder="请输入分享页域名" v-model="form.extra.share_page_domain"></el-input>
+          </el-form-item>
+          <el-form-item  label-width="120px" label="蓝云租户" prop="extra.uni_site">
+            <el-input style="width: 300px" autocomplete="off" placeholder="请输入蓝云租户" v-model="form.extra.uni_site"></el-input>
+          </el-form-item>
+          <el-form-item  label-width="120px" label="VMS租户" prop="extra.vms_site_id">
+            <el-input style="width: 300px" autocomplete="off" placeholder="请输入VMS租户" v-model="form.extra.vms_site_id"></el-input>
+          </el-form-item>
+          <el-form-item  label-width="120px" label="大数据签名" prop="extra.bigdata_settings.signature">
+            <el-input style="width: 300px" autocomplete="off" placeholder="请输入大数据签名" v-model="form.extra.bigdata_settings.signature"></el-input>
+          </el-form-item>
+          <el-form-item  label-width="120px" label="大数据租户ID" prop="extra.bigdata_settings.customer_id">
+            <el-input style="width: 300px" autocomplete="off" placeholder="请输入大数据租户ID" v-model="form.extra.bigdata_settings.customer_id"></el-input>
+          </el-form-item>
+          <el-form-item  label-width="120px" label="大数据产品ID" prop="extra.bigdata_settings.product_id">
+            <el-input style="width: 300px" autocomplete="off" placeholder="请输入大数据产品ID" v-model="form.extra.bigdata_settings.product_id"></el-input>
+          </el-form-item>
+          <el-form-item label-width="120px" label="负责人姓名" prop="site_manager_name">
+            <el-input style="width: 300px" autocomplete="off" placeholder="请输入负责人姓名"  v-model="form.site_manager_name" ></el-input>
+          </el-form-item>
+          <el-form-item label-width="120px" label="负责人手机号" prop="site_manager_phone">
+            <el-input style="width: 300px" autocomplete="off" placeholder="请输入负责人手机号" v-model="form.site_manager_phone"></el-input>
+          </el-form-item>
+          <el-form-item  label-width="120px" label="简介" prop="introduction">
+            <el-input type="textarea" style="width: 300px" autocomplete="off" placeholder="请输入简介" v-model="form.introduction"></el-input>
+          </el-form-item>
+          <el-form-item el-form-item  label-width="120px" label="是否启用" prop="status">
+            <el-select v-model="form.status" placeholder="请选择">
+              <el-option v-for="item in statusoptions" :key="item.value" :label="item.label" :value="item.value">
               </el-option>
             </el-select>
-        </el-form-item>
+          </el-form-item>
 
-        <el-form-item  label-width="120px" label="基础点击量随机" prop="extra.random_view_range">
-          <el-input-number
-          @input="forceUpdate()"
-          :precision="0"
-          :max="form.extra.random_view_range.max || 0"
-          :controls="false"
-          v-model="form.extra.random_view_range.min"></el-input-number>
-           ~
-           <el-input-number
-          @input="forceUpdate()"
-          :precision="0"
-          :min="form.extra.random_view_range.min || 0"
-          :controls="false"
-          v-model="form.extra.random_view_range.max"></el-input-number>
-          <!-- <el-input style="width: 150px" @input="forceUpdate()" oninput ="value=value.replace(/[^0-9]/g,'')" autocomplete="off" placeholder="请输入最小值" v-model="form.extra.random_view_range.min"></el-input> ~  -->
-          <!-- <el-input style="width: 150px " @input="forceUpdate()" oninput ="value=value.replace(/[^0-9]/g,'')" autocomplete="off" placeholder="请输入最大值" v-model="form.extra.random_view_range.max"></el-input> -->
-        </el-form-item>
-        <el-form-item  label-width="120px" label="点击量系数" prop="extra.multiplying_factor">
-          <el-input-number
-          @input="forceUpdate()"
-          :precision="0"
-          :controls="false"
-          v-model="form.extra.multiplying_factor"></el-input-number>
-        </el-form-item>
-        <el-form-item  label-width="120px" label="LOGO："  prop="logo">
-          <el-upload
-            class="avatar-uploader"
-            :action="VUE_APP_BASE_API+'/api/upload/image'"
-            :headers="importHeaders"
-            name="image"
-            :show-file-list="false"
-            :on-success="handleAvatarSuccess"
-            :before-upload="beforeAvatarUpload">
-            <img v-if="form.logo" :src="form.logo" class="avatar">
-            <i v-else class="el-icon-plus avatar-uploader-icon"></i>
-          </el-upload>
-        </el-form-item>
-        <el-form-item  label-width="120px" label="水印："  prop="extra.watermark">
-          <upload-single v-model="form.extra.watermark" />
-        </el-form-item>
-        <el-form-item  label-width="120px" label="分享页域名" prop="extra.share_page_domain">
-          <el-input style="width: 300px" autocomplete="off" placeholder="请输入分享页域名" v-model="form.extra.share_page_domain"></el-input>
-        </el-form-item>
-        <el-form-item  label-width="120px" label="蓝云租户" prop="extra.uni_site">
-          <el-input style="width: 300px" autocomplete="off" placeholder="请输入蓝云租户" v-model="form.extra.uni_site"></el-input>
-        </el-form-item>
-        <el-form-item  label-width="120px" label="签名" prop="extra.bigdata_settings.signature">
-          <el-input style="width: 300px" autocomplete="off" placeholder="请输入签名" v-model="form.extra.bigdata_settings.signature"></el-input>
-        </el-form-item>
-        <el-form-item  label-width="120px" label="租户ID" prop="extra.bigdata_settings.customer_id">
-          <el-input style="width: 300px" autocomplete="off" placeholder="请输入租户ID" v-model="form.extra.bigdata_settings.customer_id"></el-input>
-        </el-form-item>
-        <el-form-item  label-width="120px" label="产品ID" prop="extra.bigdata_settings.product_id">
-          <el-input style="width: 300px" autocomplete="off" placeholder="请输入产品ID" v-model="form.extra.bigdata_settings.product_id"></el-input>
-        </el-form-item>
-        <el-form-item label-width="120px" label="负责人姓名" prop="site_manager_name">
-          <el-input style="width: 300px" autocomplete="off" placeholder="请输入负责人姓名"  v-model="form.site_manager_name" ></el-input>
-        </el-form-item>
-        <el-form-item label-width="120px" label="负责人手机号" prop="site_manager_phone">
-          <el-input style="width: 300px" autocomplete="off" placeholder="请输入负责人手机号" v-model="form.site_manager_phone"></el-input>
-        </el-form-item>
-        <el-form-item  label-width="120px" label="简介" prop="introduction">
-          <el-input type="textarea" style="width: 300px" autocomplete="off" placeholder="请输入简介" v-model="form.introduction"></el-input>
-        </el-form-item>
-        <el-form-item el-form-item  label-width="120px" label="是否启用" prop="status">
-          <el-select v-model="form.status" placeholder="请选择">
-            <el-option v-for="item in statusoptions" :key="item.value" :label="item.label" :value="item.value">
-            </el-option>
-          </el-select>
-        </el-form-item>
-
-        <el-form-item el-form-item  label-width="120px" label="允许外部人员直接发布稿件" prop="extra.allow_www_publish">
-          <el-select v-model="form.extra.allow_www_publish" placeholder="请选择">
-            <el-option v-for="item in outloginoptions" :key="item.value" :label="item.label" :value="item.value">
-            </el-option>
-          </el-select>
-        </el-form-item>
+          <el-form-item el-form-item  label-width="120px" label="允许外部人员直接发布稿件" prop="extra.allow_www_publish">
+            <el-select v-model="form.extra.allow_www_publish" placeholder="请选择">
+              <el-option v-for="item in outloginoptions" :key="item.value" :label="item.label" :value="item.value">
+              </el-option>
+            </el-select>
+          </el-form-item>
+        </div>
+        
+        
       </el-form>
 
       <div class="dialog-footer" slot="footer">
@@ -328,6 +336,7 @@ import uploadSingle from '@/components/Upload/uploadSingle.vue'
             extra: {
                 watermark: '',
                 uni_site:'',
+                vms_site_id:'',
                 share_page_domain: '',
                 bigdata_settings: {
                   signature: '',
