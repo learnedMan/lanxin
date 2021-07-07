970b473ff9847349d346961c50ec0fa4955eb557
zhangyang <274684940@qq.com>
2021-07-07 14:56:17 +0800

    登录时获取新蓝云接口修改

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -268,7 +268,7 @@ export default {
               // console.log(data)
               let script=document.createElement("script");
               script.type="text/JavaScript";
-              script.src= 'http://dev-a.cztvcloud.com/admin/login?token='+ data.access_token;
+              script.src= process.env.VUE_APP_XLY_API+'/admin/login?token='+ data.access_token;
               document.getElementsByTagName('head')[0].appendChild(script);
 
               this.$router.push({ path: this.redirect || '/', query: this.otherQuery })
