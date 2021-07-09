18b33f2ae5773a7dac392abfb24a589581a5a6a6
zhangyang <274684940@qq.com>
2021-07-09 16:24:34 +0800

    模板化套餐绑定修改，新包

--- dist/static/js/chunk-2d23721c.1f7f9cdc.js
+++
@@ -1,1 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d23721c"],{fa8c:function(e,n,t){"use strict";t.r(n);var c=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",{staticStyle:{padding:"30px"}},[t("router-view")],1)},u=[],a=t("2877"),i={},l=Object(a["a"])(i,c,u,!1,null,null,null);n["default"]=l.exports}}]);
