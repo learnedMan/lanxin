764517c77467f2d29ec792b46e583238b9944c67
libin <18657190903@163.com>
2021-07-15 15:04:03 +0800

    样式修改

--- src/views/workbench/gossip/index.vue
+++ src/views/workbench/gossip/index.vue
@@ -3,8 +3,8 @@
     padding: 30px;
     &-file {
       display: inline-block;
-      width: 100px;
-      height: 100px;
+      width: 300px;
+      height: 200px;
       margin-right: 10px;
       background-color: #409eff;
     }
