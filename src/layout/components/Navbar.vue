3659156c56158b143a7b461fc1cadc3ea88c3af8
wangliangfa <1507525516@qq.com>
2021-10-12 16:52:28 +0800

    新增自定义快速访问按钮

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -59,22 +59,22 @@
         :close-on-click-modal="false"
         :close-on-press-escape="false"
         width="600px" title="快速跳转" :visible.sync="dialogShortcut">
-        <!-- <el-tree
-            :data="treedata"
-            :default-checked-keys="treechoosedata"
-            show-checkbox
-            :check-strictly="true"
-            @check = "nodeclick"
-            @check-change = "permissionMenu"
-            default-expand-all
-            node-key="id"
-            ref="roletree"
-            highlight-current
-            :props="roledefaultProps">
-          </el-tree> -->
+         <div style="height: 400px;overflow: auto;" class="shortcut">
+          <el-tree
+              :data="treedata"
+              :default-checked-keys="treechoosedata"
+              show-checkbox
+              :check-strictly="true"
+              default-expand-all
+              node-key="path"
+              ref="shortcutTree"
+              highlight-current
+              :props="roledefaultProps">
+            </el-tree>
+         </div>
         <div class="dialog-footer" slot="footer">
-          <el-button v-points = "1500" @click="closeDialog">取 消</el-button>
-          <el-button v-points = "1500" @click="enterDialog" type="primary">确 定</el-button>
+          <el-button v-points = "1500" @click="dialogShortcut = false;">取 消</el-button>
+          <el-button v-points = "1500" @click="enterShortcutDialog" type="primary">确 定</el-button>
         </div>
       </el-dialog>
     </div>
@@ -86,8 +86,9 @@ import { mapGetters } from 'vuex'
 import Breadcrumb from '@/components/Breadcrumb'
 import Hamburger from '@/components/Hamburger'
 import ErrorLog from '@/components/ErrorLog'
-
-
+import { deepClone } from '@/utils'
+import { PatchShortcuts } from '@/api/content'
+// import store from './store'
 export default {
   components: {
     Breadcrumb,
@@ -98,10 +99,12 @@ export default {
     return {
       dialogFormVisible: false,
       dialogShortcut: false,
+      treedata: [],
+      treechoosedata: [],
       roledefaultProps: {
         children: 'children',
         label: 'title',
-        id: 'url'
+        id: 'path'
       },
       form:{
         id:""
@@ -111,20 +114,20 @@ export default {
           { required: true, message: "请选择站点", trigger: "blur" }
         ]
       },
-      btns: [
-        {
-          label: '新闻审核',
-          url: '/workbench/reviewNews'
-        },
-        {
-          label: '我的稿件',
-          url: '/content/mediaAssets/my'
-        },
-        {
-          label: '直播间',
-          url: '/content/studio/studioList'
-        }
-      ]
+      // btns: [
+      //   {
+      //     label: '新闻审核',
+      //     url: '/workbench/reviewNews'
+      //   },
+      //   {
+      //     label: '我的稿件',
+      //     url: '/content/mediaAssets/my'
+      //   },
+      //   {
+      //     label: '直播间',
+      //     url: '/content/studio/studioList'
+      //   }
+      // ]
     }
   },
   computed: {
@@ -136,6 +139,9 @@ export default {
     ]),
     avatarImg () {
       return this.avatar? `${this.avatar}?imageView2/1/w/80/h/80` : require('@/assets/c_images/useravatar.jpg')
+    },
+    btns () {
+      return this.$store.state.user.u_info.extra.shortcuts
     },
     changeSite(){
       let flag = false;
@@ -160,12 +166,54 @@ export default {
     },
     /*修改快捷方式*/
     changeShortcut () {
-      // this.dialogShortcut = true
-      // console.log('路由表',  this.$store.state.permission.addRoutes)
-      let routes = this.$store.state.permission.addRoutes
-      routes.map(v=>{
-        console.log(v)
+      this.dialogShortcut = true
+      let routes = deepClone(this.$store.state.permission.addRoutes)
+      let user = this.$store.state.user.u_info
+      let arr = this.treeDataFxied(routes,'')
+      this.treechoosedata = user.extra.shortcuts.map(v=>{
+        return v.url
       })
+      this.treedata = arr
+    },
+    enterShortcutDialog () {
+      let arr = this.$refs.shortcutTree.getCheckedNodes()
+      if(!arr.length) {
+        this.$message('请勾选快速跳转路由');
+      }else if(arr.length > 4) {
+        this.$message('最多只能勾选4个');
+      }else{
+         console.log(this.$refs.shortcutTree.getCheckedNodes());
+         let id = this.$store.state.user.u_info.id
+         let shortcuts = arr.map(v=>{
+           let label = v.title,url = v.path
+           return {
+             label,url
+           }
+         })
+         console.log('shortcuts',shortcuts)
+          PatchShortcuts(id, {
+              extra: {shortcuts}
+            }).then(() => {
+              this.$message({
+                message: '修改成功',
+                type: 'success'
+              })
+              this.dialogShortcut = false
+              this.$store.dispatch('user/getuserinfo')
+              this.btns = this.$store.state.user.u_info.extra.shortcuts
+            })
+      }
+    },
+    treeDataFxied (arr,url) {
+      if (!arr.length) return []
+      arr.map(v=>{
+        v.title = v.meta.title
+        v.path = url ? url + '/' + v.path : v.path
+        if (v.children && v.children.length >0) {
+          this.treeDataFxied(v.children,v.path)
+        }
+      })
+      return arr
     },
     toggleSideBar() {
       this.$store.dispatch('app/toggleSideBar')
@@ -293,4 +341,16 @@ export default {
     }
   }
 }
+</style>
+<style lang="scss">
+  .shortcut{
+    .el-tree-node {
+      .is-leaf + .el-checkbox .el-checkbox__inner {
+        display: inline-block;
+      }
+      .el-checkbox .el-checkbox__inner {
+        display: none;
+      }
+    }
+  }
 </style>
