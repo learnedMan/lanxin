9a003dada2594409b3e29857d2ae7aff3881768d
wangliangfa <1507525516@qq.com>
2021-12-06 16:46:44 +0800

    编辑器固定头部

---
+++ src/views/content/mediaRepository/menuL.vue
@@ -0,0 +1,43 @@
+<style type="text/scss" lang="scss" scoped>
+.xl-component-menu {
+  width: 200px;
+  margin-right: 10px;
+}
+</style>
+<template>
+    <div class="xl-component-menu">
+      <el-scrollbar wrap-class="scrollbar-wrapper">
+        <el-menu
+          :default-active="activeMenu"
+          @select="handleSelect"
+        >
+          <menu-item-l v-for="menu of menus" :key="menu.code" :item="menu"></menu-item-l>
+        </el-menu>
+      </el-scrollbar>
+    </div>
+</template>
+
+<script>
+import MenuItemL from './menuItemL.vue';
+
+    export default {
+      props: {
+        menus: {
+          type: Array,
+          default: () => []
+        },
+        activeMenu: {
+          type: String,
+          default: '0'
+        }
+      },
+      components: {
+        MenuItemL
+      },
+      methods: {
+        handleSelect (key) {
+          this.$emit('select', key);
+        }
+      }
+    }
+</script>
